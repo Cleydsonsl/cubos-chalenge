fe8fd2c3400d27c493ed356bd45a28ff37c0b5c4
Cleydson Souza <cley.souza007@gmail.com>
2022-05-27 18:32:30 -0300

    start styled component seachbar

---
+++ src/components/SeachBar/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Input } from "./styles";
+
+export function SearchBar() {
+  return (
+    <Container>
+      <Input placeholder="Busque um filme por nome, ano ou gênero..."/>
+    </Container>
+  )
+}
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,11 @@
-import { Header } from '../../components/Header';
+import { SearchBar } from '../../components/SeachBar';
 import { Container } from './styled';
 
 export function Dashboard(){
   return (
     <>
       <Container>
-        <Header/>
+        <SearchBar/>
       </Container>
     </>
   );
