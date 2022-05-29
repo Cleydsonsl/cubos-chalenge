b7907dfa4210481ab11fcb66ebbd036b4cd4ab0b
Cleydson Souza <cley.souza007@gmail.com>
2022-05-29 08:08:01 -0300

    add functions to params from detailmovie

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,82 +1,61 @@
-import { Container, Content, Header, Title, Date, MovieDetails, SubTitle, Line, Text, Brand, Information, TitleInformation, ContentInformation, BoxInformation, Category, CardCategory, TextCategory, Punctuation, BoxPunctuation } from './styled';
+import { 
+  Container, 
+  Content, 
+  Header, 
+  Title, 
+  Date, 
+  MovieDetails, 
+  SubTitle, 
+  Line, 
+  Text, 
+  Brand, 
+  Information, 
+  TitleInformation, 
+  ContentInformation, 
+  BoxInformation, 
+  Category, 
+  CardCategory, 
+  TextCategory, 
+  Punctuation, 
+  BoxPunctuation } from './styled';
 
-import image from '../../assets/capa.png'
+import image from '../../assets/capa.png';
+import { useEffect, useState } from 'react';
+import { apiMovieDetail, key } from '../../services/api';
+import { useMatch } from 'react-router-dom';
+
+interface IRepoProps {
+  id: string;
+}
+
+interface IGenreProps {
+  id: string;
+  name: string;
+}
 
 export function Details() {
+  const params  = useMatch('');
+  const [repository, setRepository] = useState<any>(null);
+
+  useEffect(() => {
+    apiMovieDetail.get(`${params}?api_key=${key}&language=pt-BR&append_to_response=videos`,
+    ).then(response => setRepository(response.data));
+  }, [params]);
+
+  console.log(repository);
+
+  function handleHours(minutes: number) {
+    const hour = minutes / 60;
+    let p = hour.toString().replace('.', 'h');
+    let newHour = p.substr(0, 4);
+    console.log(newHour);
+
+    return newHour;
+  }
+
   return (
     <Container>
-      <Header>
-        <Title>
-          Thor: Ragnarok
-        </Title>
-        <Date>
-          25/10/2017
-        </Date>
-      </Header>
-      <Content>
-        <MovieDetails>
-            <SubTitle>
-              Sinopse
-            </SubTitle>
-            <Line/>
-            <Text>
-              Thor esta aprisionado do outo lado do universo, sem seu martelo, e se vê em uma corrida para voltar até Asgard e impedir o Ragnarok, a destruição de seu lar e o fim da civilização asgardiana que está nas mãos de uma nova e poderosa ameaça, a terrível Hela. Mas primeiro ele precisa sobreviver a uma batalha de gladiadores que o coloca contra seu ex-aliado e vingador, o Incrível Hulk.
-            </Text>
-            <SubTitle>
-              Informações
-            </SubTitle>
-            <Line/>
-            <Information>
-              <BoxInformation>
-                <TitleInformation> Situação </TitleInformation>
-                <ContentInformation>Lançado</ContentInformation>
-              </BoxInformation>
-              <BoxInformation>
-                <TitleInformation> Idioma </TitleInformation>
-                <ContentInformation>Inglês</ContentInformation>
-              </BoxInformation>
-              <BoxInformation>
-                <TitleInformation> Duração </TitleInformation>
-                <ContentInformation>2h 10min</ContentInformation>
-              </BoxInformation>
-              <BoxInformation>
-                <TitleInformation> Orçamento </TitleInformation>
-                <ContentInformation>$180.000.000,00</ContentInformation>
-              </BoxInformation>
-              <BoxInformation>
-                <TitleInformation> Receita </TitleInformation>
-                <ContentInformation>$853.977.000,00</ContentInformation>
-              </BoxInformation>
-              <BoxInformation>
-                <TitleInformation> Lucro </TitleInformation>
-                <ContentInformation>$853.977.000,00</ContentInformation>
-              </BoxInformation>
-            </Information>
-            <Category>
-              <CardCategory>
-                <TextCategory>
-                    Ação
-                </TextCategory>
-              </CardCategory>
-              <CardCategory>
-                <TextCategory>
-                    Aventura
-                </TextCategory>
-              </CardCategory>
-              <CardCategory>
-                <TextCategory>
-                    Fantasia
-                </TextCategory>
-              </CardCategory>
-            </Category>
-            <BoxPunctuation>
-              <Punctuation>
-                75%
-              </Punctuation>
-            </BoxPunctuation>
-        </MovieDetails>
-        <Brand src={ image }/>
-      </Content>
+      
     </Container>
   )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const key = 'a9a6bc97baae4f51dd8ed8e36b7a2ca9';
+export const key = 'a9a6bc97baae4f51dd8ed8e36b7a2ca9';
 
 export const api = axios.create({
   baseURL: `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${key}&page=1`
